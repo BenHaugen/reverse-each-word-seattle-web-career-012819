1b4359f22466fa626975fac2b577c868b4f5e729
Ben Haugen <ben1232jazz@hotmail.com>
2019-01-24 00:23:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   new_array = string.split(" ")
   array = []
   new_array.each do |word|
-  array.push(new_array.reverse)
+  array.push(word.reverse)
 end
 return array.join(" ")
 end
